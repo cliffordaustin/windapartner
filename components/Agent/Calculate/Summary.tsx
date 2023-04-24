e8a5a4f926ec63a40ce6aa161fa7695b87e2200d
cliffordaustin <cliffordaustin670@gmail.com>
2023-04-24 06:04:07 +0000

    first commit

--- components/Agent/Calculate/Summary.tsx
+++ components/Agent/Calculate/Summary.tsx
@@ -223,246 +223,12 @@ export default function Summary({ calculateStay, stays }: SummaryProps) {
   );
 
   return (
-    <Tabs color="red" variant="outline" defaultValue={"resident"}>
+    <Tabs color="red" variant="outline" defaultValue={"non-resident"}>
       <Tabs.List grow>
-        <Tabs.Tab value="resident">Resident</Tabs.Tab>
         <Tabs.Tab value="non-resident">Non-resident</Tabs.Tab>
+        <Tabs.Tab value="resident">Resident</Tabs.Tab>
       </Tabs.List>
 
-      <Tabs.Panel value="resident">
-        <div className="relative">
-          <div className="w-full px-4 py-4">
-            <Text className="font-bold" mb={8}>
-              {calculateStay.name}
-            </Text>
-            {calculateStay.date[0] && calculateStay.date[1] && (
-              <>
-                <div className="flex justify-between gap-8">
-                  <div className="flex border w-[45%] flex-col gap-2">
-                    <div className="flex gap-2 items-center">
-                      <IconCalendarEvent></IconCalendarEvent>
-                      <Text size="sm">Check-in</Text>
-                    </div>
-
-                    <Text size="sm" weight={700} ml={4}>
-                      {moment(
-                        calculateStay.date[0]?.toLocaleDateString()
-                      ).format("DD MMM YYYY")}
-                    </Text>
-                  </div>
-
-                  <div className="flex w-[45%] flex-col gap-2">
-                    <div className="flex gap-2 items-center">
-                      <IconCalendarEvent></IconCalendarEvent>
-                      <Text size="sm">Check-out</Text>
-                    </div>
-
-                    <Text size="sm" weight={700} ml={4}>
-                      {moment(
-                        calculateStay.date[1]?.toLocaleDateString()
-                      ).format("DD MMM YYYY")}
-                    </Text>
-                  </div>
-                </div>
-
-                <Text
-                  size="sm"
-                  weight={600}
-                  className="mt-2 border border-solid w-fit px-2 border-gray-200"
-                >
-                  {nights} {nights > 1 ? "nights" : "night"}
-                </Text>
-                <Divider size="xs" mt={8}></Divider>
-              </>
-            )}
-
-            {calculateStay.date[0] &&
-              calculateStay.date[1] &&
-              countRoomType.length > 0 && (
-                <div className="flex flex-col gap-2">
-                  <Text size="sm" weight={700} className="mt-2">
-                    Rooms
-                  </Text>
-
-                  <Flex direction="column" gap={2}>
-                    {countRoomType.map((item, index) => (
-                      <Text size="sm" key={index}>
-                        {item}
-                      </Text>
-                    ))}
-                  </Flex>
-                </div>
-              )}
-
-            {calculateStay.date[0] &&
-              calculateStay.date[1] &&
-              calculateStay.rooms[0].residentGuests.length > 0 && (
-                <div className="flex flex-col gap-2">
-                  <Flex justify="space-between" className="mt-2" align="center">
-                    <Text size="sm" weight={700}>
-                      Guests
-                    </Text>
-
-                    <Text size="sm" weight={600}>
-                      {totalResidentPrice
-                        ? `KES ${totalResidentPrice.toLocaleString()}`
-                        : ""}{" "}
-                    </Text>
-                  </Flex>
-
-                  <Flex direction="column" gap={2}>
-                    {calculateStay.rooms.map((room, index) => (
-                      <GuestsSummary
-                        key={index}
-                        room={room}
-                        index={index}
-                        roomTypes={roomTypes}
-                      ></GuestsSummary>
-                    ))}
-                  </Flex>
-                </div>
-              )}
-
-            {((calculateStay.date[0] &&
-              calculateStay.date[1] &&
-              calculateStay.rooms[0].residentParkFee.length > 0) ||
-              (calculateStay.date[0] &&
-                calculateStay.date[1] &&
-                calculateStay.rooms[0].nonResidentParkFee.length > 0)) && (
-              <div className="flex flex-col gap-2">
-                <Divider size="xs" mt={8}></Divider>
-                <Flex justify="space-between" align="center">
-                  <Text size="sm" weight={700}>
-                    Fees
-                  </Text>
-
-                  <Text size="sm" weight={600}>
-                    {feePrice.residentTotalFeePrice
-                      ? `KES ${feePrice.residentTotalFeePrice.toLocaleString()}`
-                      : ""}{" "}
-                  </Text>
-                </Flex>
-
-                <Flex direction="column" gap={2}>
-                  {calculateStay.rooms.map((room, index) => (
-                    <FeesSummary
-                      key={index}
-                      room={room}
-                      index={index}
-                    ></FeesSummary>
-                  ))}
-                </Flex>
-              </div>
-            )}
-
-            {calculateStay.date[0] &&
-              calculateStay.date[1] &&
-              calculateStay.activityFee.length > 0 && (
-                <div className="flex flex-col gap-2">
-                  <Divider size="xs" mt={8}></Divider>
-                  <Flex justify="space-between" align="center">
-                    <Text size="sm" weight={700}>
-                      Activities
-                    </Text>
-
-                    <Text size="sm" weight={600}>
-                      {activityTotalPrice
-                        ? `$ ${activityTotalPrice.toLocaleString()}`
-                        : ""}
-                    </Text>
-                  </Flex>
-
-                  <Flex direction="column" gap={2}>
-                    {calculateStay.activityFee.map((activity, index) => (
-                      <ActivitiesSummary
-                        key={index}
-                        activity={activity}
-                        numberOfGuests={
-                          totalNumberOfGuests + totalNumberOfNonResidentGuests
-                        }
-                        nights={nights}
-                      ></ActivitiesSummary>
-                    ))}
-                  </Flex>
-                </div>
-              )}
-
-            {calculateStay.date[0] &&
-              calculateStay.date[1] &&
-              calculateStay.extraFee[0].name && (
-                <div className="flex flex-col gap-2">
-                  <Divider size="xs" mt={8}></Divider>
-                  <Flex justify="space-between" align="center">
-                    <Text size="sm" weight={700} className="">
-                      Extra Fees
-                    </Text>
-
-                    <Text size="sm" weight={600}>
-                      {totalResidentExtraFees
-                        ? `KES ${totalResidentExtraFees.toLocaleString()}`
-                        : ""}{" "}
-                    </Text>
-                  </Flex>
-
-                  <Flex direction="column" gap={2}>
-                    {residentExtraFees.map((fee, index) => (
-                      <ExtraFeesSummary
-                        key={index}
-                        index={index}
-                        fee={fee}
-                        numberOfGuests={totalNumberOfGuests}
-                        nights={nights}
-                      ></ExtraFeesSummary>
-                    ))}
-                  </Flex>
-                </div>
-              )}
-
-            {calculateStay.residentCommission && (
-              <div className="flex flex-col gap-2">
-                <Divider size="xs" mt={8}></Divider>
-
-                <Flex justify="space-between" align="center">
-                  <Text size="sm" weight={700} className="">
-                    Resident Commission
-                  </Text>
-
-                  <Text size="sm" weight={600}>
-                    {calculateStay.residentCommission}%
-                  </Text>
-                </Flex>
-              </div>
-            )}
-
-            {(!calculateStay.date[0] || !calculateStay.date[1]) && (
-              <div className="flex flex-col mt-12 mx-auto items-center gap-2">
-                <IconCalculator></IconCalculator>
-                <Text size={"sm"} className="text-gray-600 text-center">
-                  Enter your dates to start calculating.
-                </Text>
-              </div>
-            )}
-          </div>
-
-          {!!calculateStay.date[0] &&
-            !!calculateStay.date[1] &&
-            !!calculateStay.rooms[0].name &&
-            !!residentFullTotalPrice && (
-              <div className="sticky flex items-center py-1 px-2 left-4 bottom-0 w-full bg-white border-x-transparent border-b-transparent shadow-md border-t border-t-gray-200 border-solid h-[60px]">
-                <div className="flex flex-col">
-                  <Text className="text-gray-400" size="xs">
-                    TOTAL
-                  </Text>
-                  <Text size="lg" weight={700}>
-                    {residentFullTotalPrice
-                      ? `KES ${residentFullTotalPrice.toLocaleString()}`
-                      : ""}{" "}
-                  </Text>
-                </div>
-              </div>
-            )}
-        </div>
-      </Tabs.Panel>
       <Tabs.Panel value="non-resident">
         <div className="relative">
           <div className="w-full px-4 py-4">
@@ -691,6 +457,240 @@ export default function Summary({ calculateStay, stays }: SummaryProps) {
                     {nonResidentFullTotalPrice
                       ? `$ ${nonResidentFullTotalPrice.toLocaleString()}`
                       : ""}
+                  </Text>
+                </div>
+              </div>
+            )}
+        </div>
+      </Tabs.Panel>
+      <Tabs.Panel value="resident">
+        <div className="relative">
+          <div className="w-full px-4 py-4">
+            <Text className="font-bold" mb={8}>
+              {calculateStay.name}
+            </Text>
+            {calculateStay.date[0] && calculateStay.date[1] && (
+              <>
+                <div className="flex justify-between gap-8">
+                  <div className="flex border w-[45%] flex-col gap-2">
+                    <div className="flex gap-2 items-center">
+                      <IconCalendarEvent></IconCalendarEvent>
+                      <Text size="sm">Check-in</Text>
+                    </div>
+
+                    <Text size="sm" weight={700} ml={4}>
+                      {moment(
+                        calculateStay.date[0]?.toLocaleDateString()
+                      ).format("DD MMM YYYY")}
+                    </Text>
+                  </div>
+
+                  <div className="flex w-[45%] flex-col gap-2">
+                    <div className="flex gap-2 items-center">
+                      <IconCalendarEvent></IconCalendarEvent>
+                      <Text size="sm">Check-out</Text>
+                    </div>
+
+                    <Text size="sm" weight={700} ml={4}>
+                      {moment(
+                        calculateStay.date[1]?.toLocaleDateString()
+                      ).format("DD MMM YYYY")}
+                    </Text>
+                  </div>
+                </div>
+
+                <Text
+                  size="sm"
+                  weight={600}
+                  className="mt-2 border border-solid w-fit px-2 border-gray-200"
+                >
+                  {nights} {nights > 1 ? "nights" : "night"}
+                </Text>
+                <Divider size="xs" mt={8}></Divider>
+              </>
+            )}
+
+            {calculateStay.date[0] &&
+              calculateStay.date[1] &&
+              countRoomType.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <Text size="sm" weight={700} className="mt-2">
+                    Rooms
+                  </Text>
+
+                  <Flex direction="column" gap={2}>
+                    {countRoomType.map((item, index) => (
+                      <Text size="sm" key={index}>
+                        {item}
+                      </Text>
+                    ))}
+                  </Flex>
+                </div>
+              )}
+
+            {calculateStay.date[0] &&
+              calculateStay.date[1] &&
+              calculateStay.rooms[0].residentGuests.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <Flex justify="space-between" className="mt-2" align="center">
+                    <Text size="sm" weight={700}>
+                      Guests
+                    </Text>
+
+                    <Text size="sm" weight={600}>
+                      {totalResidentPrice
+                        ? `KES ${totalResidentPrice.toLocaleString()}`
+                        : ""}{" "}
+                    </Text>
+                  </Flex>
+
+                  <Flex direction="column" gap={2}>
+                    {calculateStay.rooms.map((room, index) => (
+                      <GuestsSummary
+                        key={index}
+                        room={room}
+                        index={index}
+                        roomTypes={roomTypes}
+                      ></GuestsSummary>
+                    ))}
+                  </Flex>
+                </div>
+              )}
+
+            {((calculateStay.date[0] &&
+              calculateStay.date[1] &&
+              calculateStay.rooms[0].residentParkFee.length > 0) ||
+              (calculateStay.date[0] &&
+                calculateStay.date[1] &&
+                calculateStay.rooms[0].nonResidentParkFee.length > 0)) && (
+              <div className="flex flex-col gap-2">
+                <Divider size="xs" mt={8}></Divider>
+                <Flex justify="space-between" align="center">
+                  <Text size="sm" weight={700}>
+                    Fees
+                  </Text>
+
+                  <Text size="sm" weight={600}>
+                    {feePrice.residentTotalFeePrice
+                      ? `KES ${feePrice.residentTotalFeePrice.toLocaleString()}`
+                      : ""}{" "}
+                  </Text>
+                </Flex>
+
+                <Flex direction="column" gap={2}>
+                  {calculateStay.rooms.map((room, index) => (
+                    <FeesSummary
+                      key={index}
+                      room={room}
+                      index={index}
+                    ></FeesSummary>
+                  ))}
+                </Flex>
+              </div>
+            )}
+
+            {calculateStay.date[0] &&
+              calculateStay.date[1] &&
+              calculateStay.activityFee.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <Divider size="xs" mt={8}></Divider>
+                  <Flex justify="space-between" align="center">
+                    <Text size="sm" weight={700}>
+                      Activities
+                    </Text>
+
+                    <Text size="sm" weight={600}>
+                      {activityTotalPrice
+                        ? `$ ${activityTotalPrice.toLocaleString()}`
+                        : ""}
+                    </Text>
+                  </Flex>
+
+                  <Flex direction="column" gap={2}>
+                    {calculateStay.activityFee.map((activity, index) => (
+                      <ActivitiesSummary
+                        key={index}
+                        activity={activity}
+                        numberOfGuests={
+                          totalNumberOfGuests + totalNumberOfNonResidentGuests
+                        }
+                        nights={nights}
+                      ></ActivitiesSummary>
+                    ))}
+                  </Flex>
+                </div>
+              )}
+
+            {calculateStay.date[0] &&
+              calculateStay.date[1] &&
+              calculateStay.extraFee[0].name && (
+                <div className="flex flex-col gap-2">
+                  <Divider size="xs" mt={8}></Divider>
+                  <Flex justify="space-between" align="center">
+                    <Text size="sm" weight={700} className="">
+                      Extra Fees
+                    </Text>
+
+                    <Text size="sm" weight={600}>
+                      {totalResidentExtraFees
+                        ? `KES ${totalResidentExtraFees.toLocaleString()}`
+                        : ""}{" "}
+                    </Text>
+                  </Flex>
+
+                  <Flex direction="column" gap={2}>
+                    {residentExtraFees.map((fee, index) => (
+                      <ExtraFeesSummary
+                        key={index}
+                        index={index}
+                        fee={fee}
+                        numberOfGuests={totalNumberOfGuests}
+                        nights={nights}
+                      ></ExtraFeesSummary>
+                    ))}
+                  </Flex>
+                </div>
+              )}
+
+            {calculateStay.residentCommission && (
+              <div className="flex flex-col gap-2">
+                <Divider size="xs" mt={8}></Divider>
+
+                <Flex justify="space-between" align="center">
+                  <Text size="sm" weight={700} className="">
+                    Resident Commission
+                  </Text>
+
+                  <Text size="sm" weight={600}>
+                    {calculateStay.residentCommission}%
+                  </Text>
+                </Flex>
+              </div>
+            )}
+
+            {(!calculateStay.date[0] || !calculateStay.date[1]) && (
+              <div className="flex flex-col mt-12 mx-auto items-center gap-2">
+                <IconCalculator></IconCalculator>
+                <Text size={"sm"} className="text-gray-600 text-center">
+                  Enter your dates to start calculating.
+                </Text>
+              </div>
+            )}
+          </div>
+
+          {!!calculateStay.date[0] &&
+            !!calculateStay.date[1] &&
+            !!calculateStay.rooms[0].name &&
+            !!residentFullTotalPrice && (
+              <div className="sticky flex items-center py-1 px-2 left-4 bottom-0 w-full bg-white border-x-transparent border-b-transparent shadow-md border-t border-t-gray-200 border-solid h-[60px]">
+                <div className="flex flex-col">
+                  <Text className="text-gray-400" size="xs">
+                    TOTAL
+                  </Text>
+                  <Text size="lg" weight={700}>
+                    {residentFullTotalPrice
+                      ? `KES ${residentFullTotalPrice.toLocaleString()}`
+                      : ""}{" "}
                   </Text>
                 </div>
               </div>
