32a4002e13af83d875c861eb32ac2ea47b400376
cliffordaustin <cliffordaustin670@gmail.com>
2023-06-28 12:20:00 +0000

    changes in code

--- components/Lodge/LodgeCard.tsx
+++ components/Lodge/LodgeCard.tsx
@@ -16,11 +16,18 @@ import {
   createStyles,
   FileInputProps,
   rem,
+  Box,
+  ActionIcon,
+  Popover,
+  Menu,
+  NumberInput,
+  Select,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import {
   IconChevronLeft,
   IconChevronRight,
+  IconDots,
   IconPencil,
   IconPhoto,
   IconPlus,
@@ -161,6 +168,44 @@ function LodgeCard({ stay, stayIds, setStayIds }: LodgeProps) {
     );
   }
 
+  type ParkFeesValues = {
+    name: string | "";
+    adultPrice: number | "";
+    childPrice: number | "";
+    teenPrice: number | "";
+    nonResidentAdultPrice: number | "";
+    nonResidentChildPrice: number | "";
+    nonResidentTeenPrice: number | "";
+  };
+
+  const parkFeesForm = useForm<ParkFeesValues>({
+    initialValues: {
+      name: "",
+      adultPrice: "",
+      childPrice: "",
+      teenPrice: "",
+      nonResidentAdultPrice: "",
+      nonResidentChildPrice: "",
+      nonResidentTeenPrice: "",
+    },
+  });
+
+  type ActivityValues = {
+    name: string | "";
+    description: string | "";
+    residentPrice: number | "";
+    nonResidentPrice: number | "";
+  };
+
+  const activityForm = useForm<ActivityValues>({
+    initialValues: {
+      name: "",
+      description: "",
+      residentPrice: "",
+      nonResidentPrice: "",
+    },
+  });
+
   const ValueComponent: FileInputProps["valueComponent"] = ({ value }) => {
     if (Array.isArray(value)) {
       return (
@@ -220,6 +265,60 @@ function LodgeCard({ stay, stayIds, setStayIds }: LodgeProps) {
     }
   );
 
+  const [openParkFeesModal, { open: openParkFees, close: closeParkFees }] =
+    useDisclosure(false);
+
+  const [parkFeeLoading, setParkFeeLoading] = React.useState(false);
+  const addParkFees = async (values: ParkFeesValues) => {};
+
+  const [openActivityModal, { open: openActivity, close: closeActivity }] =
+    useDisclosure(false);
+
+  const [activityOption, setActivityOption] = React.useState<string | null>(
+    "PER PERSON"
+  );
+
+  const [activityLoading, setActivityLoading] = React.useState(false);
+
+  const addActivity = async (values: ActivityValues) => {};
+
+  const [
+    openAddContractRateModal,
+    { open: openAddContractRate, close: closeAddContractRate },
+  ] = useDisclosure(false);
+
+  const [contractRateFile, setContractRateFile] = React.useState<File | null>(
+    null
+  );
+
+  const [contractRateLoading, setContractRateLoading] = React.useState(false);
+
+  const [noContractRateFile, setNoContractRateFile] = React.useState(false);
+
+  const addContractRate = async () => {
+    if (!contractRateFile) {
+      setNoContractRateFile(true);
+      return;
+    } else {
+      setNoContractRateFile(false);
+      setContractRateLoading(true);
+      const formData = new FormData();
+      formData.append("lodge_price_data_pdf", contractRateFile);
+
+      await axios.patch(
+        `${process.env.NEXT_PUBLIC_baseURL}/user-stays-email/${stay.slug}/`,
+        formData,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+
+      router.reload();
+    }
+  };
+
   return (
     <div className="w-full rounded-md relative shadow border">
       <Carousel
@@ -266,16 +365,283 @@ function LodgeCard({ stay, stayIds, setStayIds }: LodgeProps) {
           {stay.location}
         </Text>
 
-        <Button
-          size="xs"
-          color="red"
-          variant="light"
-          className="w-full mt-1 !py-2"
-          onClick={open}
-        >
-          Add rates
-        </Button>
+        <Box sx={{ display: "flex", gap: 6, alignItems: "center" }}>
+          <Button
+            size="xs"
+            color="red"
+            variant="light"
+            className="w-full mt-1 !py-2"
+            onClick={open}
+          >
+            Add rates
+          </Button>
+
+          <Menu
+            trigger="hover"
+            openDelay={100}
+            closeDelay={400}
+            shadow="md"
+            width={250}
+          >
+            <Menu.Target>
+              <ActionIcon size="md" className="bg-gray-200 hover:bg-gray-300">
+                <IconDots size="1rem" />
+              </ActionIcon>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Fees</Menu.Label>
+              <Menu.Item onClick={openParkFees}>
+                Add Park/Conservancy fee
+              </Menu.Item>
+              <Menu.Item onClick={openActivity}>Add Activities</Menu.Item>
+
+              <Menu.Divider />
+
+              <Menu.Label>Rates</Menu.Label>
+              <Menu.Item onClick={openAddContractRate}>
+                Add contact rates
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Box>
       </div>
+
+      <Modal
+        opened={openParkFeesModal}
+        onClose={closeParkFees}
+        title={"Park/Conservancy fees"}
+        classNames={{
+          title: "text-lg font-bold",
+          close: "text-black hover:text-gray-700 hover:bg-gray-200",
+          header: "bg-gray-100",
+        }}
+        transitionProps={{ transition: "fade", duration: 200 }}
+        closeButtonProps={{
+          style: {
+            width: 30,
+            height: 30,
+          },
+          iconSize: 20,
+        }}
+      >
+        <form
+          className="flex flex-col gap-1"
+          onSubmit={parkFeesForm.onSubmit((values) => addParkFees(values))}
+        >
+          <TextInput
+            label="Fee name"
+            placeholder="Enter fee name"
+            value={parkFeesForm.values.name}
+            onChange={(event) =>
+              parkFeesForm.setFieldValue("name", event.currentTarget.value)
+            }
+            required
+          />
+
+          <NumberInput
+            label="Resident adult fee"
+            placeholder="Enter resident adult fee"
+            value={parkFeesForm.values.adultPrice}
+            onChange={(value) =>
+              parkFeesForm.setFieldValue("adultPrice", value)
+            }
+          />
+
+          <NumberInput
+            label="Resident teen fee"
+            placeholder="Enter resident teen fee"
+            value={parkFeesForm.values.teenPrice}
+            onChange={(value) => parkFeesForm.setFieldValue("teenPrice", value)}
+          />
+
+          <NumberInput
+            label="Resident child fee"
+            placeholder="Enter resident child fee"
+            value={parkFeesForm.values.childPrice}
+            onChange={(value) =>
+              parkFeesForm.setFieldValue("childPrice", value)
+            }
+          />
+
+          <NumberInput
+            label="Non-resident adult fee"
+            placeholder="Enter non-resident adult fee"
+            value={parkFeesForm.values.nonResidentAdultPrice}
+            onChange={(value) =>
+              parkFeesForm.setFieldValue("nonResidentAdultPrice", value)
+            }
+          />
+
+          <NumberInput
+            label="Non-resident teen fee"
+            placeholder="Enter non-resident teen fee"
+            value={parkFeesForm.values.nonResidentTeenPrice}
+            onChange={(value) =>
+              parkFeesForm.setFieldValue("nonResidentTeenPrice", value)
+            }
+          />
+
+          <NumberInput
+            label="Non-resident child fee"
+            placeholder="Enter non-resident child fee"
+            value={parkFeesForm.values.nonResidentChildPrice}
+            onChange={(value) =>
+              parkFeesForm.setFieldValue("nonResidentChildPrice", value)
+            }
+          />
+
+          <Flex gap={8} justify="right" mt={6}>
+            <Button onClick={closeParkFees} variant="default">
+              Close
+            </Button>
+            <Button disabled={parkFeeLoading} type="submit">
+              Submit{" "}
+              {parkFeeLoading && (
+                <Loader size="xs" color="gray" ml={5}></Loader>
+              )}
+            </Button>
+          </Flex>
+        </form>
+      </Modal>
+
+      <Modal
+        opened={openAddContractRateModal}
+        onClose={closeAddContractRate}
+        title={"Add Contract Rates"}
+        classNames={{
+          title: "text-lg font-bold",
+          close: "text-black hover:text-gray-700 hover:bg-gray-200",
+          header: "bg-gray-100",
+        }}
+        transitionProps={{ transition: "fade", duration: 200 }}
+        closeButtonProps={{
+          style: {
+            width: 30,
+            height: 30,
+          },
+          iconSize: 20,
+        }}
+      >
+        <FileInput
+          label="Upload contract rates"
+          placeholder="Accepted file types: PDF"
+          accept="application/pdf"
+          icon={<IconUpload size={rem(14)} />}
+          error={noContractRateFile ? "Please select at least one file" : ""}
+          onChange={(payload: File) => {
+            setNoContractRateFile(false);
+            setContractRateFile(payload);
+          }}
+        />
+
+        <Flex gap={8} justify="right" mt={6}>
+          <Button onClick={closeAddContractRate} variant="default">
+            Close
+          </Button>
+          <Button
+            disabled={contractRateLoading}
+            onClick={() => {
+              addContractRate();
+            }}
+          >
+            Submit{" "}
+            {contractRateLoading && (
+              <Loader size="xs" color="gray" ml={5}></Loader>
+            )}
+          </Button>
+        </Flex>
+      </Modal>
+
+      <Modal
+        opened={openActivityModal}
+        onClose={closeActivity}
+        title={"Activities"}
+        classNames={{
+          title: "text-lg font-bold",
+          close: "text-black hover:text-gray-700 hover:bg-gray-200",
+          header: "bg-gray-100",
+        }}
+        transitionProps={{ transition: "fade", duration: 200 }}
+        closeButtonProps={{
+          style: {
+            width: 30,
+            height: 30,
+          },
+          iconSize: 20,
+        }}
+      >
+        <form
+          className="flex flex-col gap-1"
+          onSubmit={activityForm.onSubmit((values) => addActivity(values))}
+        >
+          <TextInput
+            label="Activity name"
+            placeholder="Enter activity name"
+            value={activityForm.values.name}
+            onChange={(event) =>
+              activityForm.setFieldValue("name", event.currentTarget.value)
+            }
+            required
+          />
+
+          <TextInput
+            label="Activity description"
+            placeholder="Enter activity description"
+            value={activityForm.values.description}
+            onChange={(event) =>
+              activityForm.setFieldValue(
+                "description",
+                event.currentTarget.value
+              )
+            }
+          />
+
+          <Select
+            label="Select the pricing type"
+            placeholder="Select the pricing type"
+            clearable
+            value={activityOption}
+            onChange={(value) => setActivityOption(value)}
+            data={[
+              { label: "Per Person", value: "PER PERSON" },
+              { label: "Per Person Per Night", value: "PER PERSON PER NIGHT" },
+              { label: "Whole Group", value: "WHOLE GROUP" },
+            ]}
+            required
+          />
+
+          <NumberInput
+            label="Resident Activity Price"
+            placeholder="Enter resident activity price"
+            value={activityForm.values.residentPrice}
+            onChange={(value) =>
+              activityForm.setFieldValue("residentPrice", value)
+            }
+          />
+
+          <NumberInput
+            label="Non-resident Activity Price"
+            placeholder="Enter non-resident activity price"
+            value={activityForm.values.nonResidentPrice}
+            onChange={(value) =>
+              activityForm.setFieldValue("nonResidentPrice", value)
+            }
+          />
+
+          <Flex gap={8} justify="right" mt={6}>
+            <Button onClick={closeActivity} variant="default">
+              Close
+            </Button>
+            <Button disabled={activityLoading} type="submit">
+              Submit{" "}
+              {activityLoading && (
+                <Loader size="xs" color="gray" ml={5}></Loader>
+              )}
+            </Button>
+          </Flex>
+        </form>
+      </Modal>
 
       <Modal
         opened={editModal}
