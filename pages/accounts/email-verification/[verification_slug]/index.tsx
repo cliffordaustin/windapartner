5e6abe58f7ba7df58d4db2e940e0273a7d89b036
cliffordaustin <cliffordaustin670@gmail.com>
2023-08-30 18:05:33 +0000

    changes in code

--- pages/accounts/email-verification/[verification_slug]/index.tsx
+++ pages/accounts/email-verification/[verification_slug]/index.tsx
@@ -4,6 +4,14 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { GetServerSideProps } from "next";
 import Cookies from "js-cookie";
+import Image from "next/image";
+import { Button, PasswordInput, Stack, Text, TextInput } from "@mantine/core";
+import { useForm, yupResolver } from "@mantine/form";
+import * as Yup from "yup";
+
+const getCharacterValidationError = (str: string) => {
+  return `Your password must have at least 1 ${str} character`;
+};
 
 function VerificationLogin({
   error,
@@ -19,14 +27,56 @@ function VerificationLogin({
     Cookies.remove("email-verification");
   }, []);
 
-  useEffect(() => {
-    if (!error) {
-      router.replace((router.query.redirect as string) || "/partner/agent");
-    }
-  }, [error, router, router.query.redirect]);
+  const schema = Yup.object().shape({
+    password: Yup.string()
+      // check minimum characters
+      .min(8, "Password must have at least 8 characters")
+      // different error messages for different requirements
+      .matches(/[0-9]/, getCharacterValidationError("digit"))
+      .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+      .matches(/[A-Z]/, getCharacterValidationError("uppercase"))
+      .required("Password is required"),
+    retypePassword: Yup.string().oneOf(
+      [Yup.ref("password"), ""],
+      "Passwords must match"
+    ),
+  });
+
+  const form = useForm({
+    initialValues: {
+      first_name: "",
+      last_name: "",
+      password: "",
+      retypePassword: "",
+    },
+
+    validate: yupResolver(schema),
+  });
+
+  // useEffect(() => {
+  //   if (!error) {
+  //     router.replace((router.query.redirect as string) || "/partner/agent");
+  //   }
+  // }, [error, router, router.query.redirect]);
 
   return (
     <>
+      <div className="border-b border-x-0 w-full h-[70px] border-t-0 flex items-center bg-gray-50 justify-between px-6 border-solid border-b-gray-200">
+        <div className="relative w-28 h-9 cursor-pointer">
+          <Image
+            alt="Winda logo"
+            src="/images/winda_logo/horizontal-blue-font.png"
+            className="w-full h-full"
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+            fill
+            priority
+          />
+        </div>
+
+        <div></div>
+      </div>
       {!error && (
         <div className="relative">
           <div className="w-full font-bold bg-[#0A1A44] text-center py-2 px-2 text-white">
@@ -40,57 +90,130 @@ function VerificationLogin({
           <span className="text-gray-600">An error has occurred</span>
         </div>
       )}
+
+      <div className="flex flex-col w-[400px] items-center overflow-y-scroll mt-[40px] justify-center">
+        <div className="w-full">
+          <div className="flex flex-col gap-2">
+            <Text className="text-2xl mb-4" weight={700}>
+              Email verified
+            </Text>
+
+            <Text color="dimmed" size="sm">
+              Thank you for confirming your email address.
+            </Text>
+
+            <Text color="dimmed" size="sm"></Text>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="flex flex-col gap-2">
+            <Text className="text-2xl mb-4" weight={700}>
+              Create Password
+            </Text>
+
+            <Text color="dimmed" size="sm">
+              Create a new password to secure your account
+            </Text>
+          </div>
+
+          <form onSubmit={form.onSubmit(() => {})}>
+            <Stack spacing={4}>
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                size="lg"
+                mt={4}
+                value={form.values.password}
+                onChange={(event) =>
+                  form.setFieldValue("password", event.currentTarget.value)
+                }
+                error={form.errors.password}
+                radius="sm"
+              />
+
+              <PasswordInput
+                required
+                label="Re-type new password"
+                placeholder="Re-type new password"
+                value={form.values.retypePassword}
+                onChange={(event) =>
+                  form.setFieldValue(
+                    "retypePassword",
+                    event.currentTarget.value
+                  )
+                }
+                error={form.errors.retypePassword}
+                radius="md"
+              />
+            </Stack>
+
+            <div className="flex items-center gap-3 mt-4 flex-col">
+              <Button
+                color="red"
+                type="submit"
+                size="lg"
+                radius="sm"
+                className="w-full"
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  res,
-  query,
-  resolvedUrl,
-}) => {
-  let notFound = false;
-  let serverErr = false;
-  let token = "";
-  await axios
-    .post(
-      `${process.env.NEXT_PUBLIC_baseURL}/account-confirm-email/${query.verification_slug}/`,
-      {
-        key: query.verification_slug,
-      }
-    )
-    .then((res) => {
-      token = res.data.key;
-    })
-    .catch((error: AxiosError) => {
-      if (error.response?.status === 404) {
-        notFound = true;
-      } else {
-        serverErr = true;
-      }
-    });
+// export const getServerSideProps: GetServerSideProps = async ({
+//   req,
+//   res,
+//   query,
+//   resolvedUrl,
+// }) => {
+//   let notFound = false;
+//   let serverErr = false;
+//   let token = "";
+//   await axios
+//     .post(
+//       `${process.env.NEXT_PUBLIC_baseURL}/account-confirm-email/${query.verification_slug}/`,
+//       {
+//         key: query.verification_slug,
+//       }
+//     )
+//     .then((res) => {
+//       token = res.data.key;
+//     })
+//     .catch((error: AxiosError) => {
+//       if (error.response?.status === 404) {
+//         notFound = true;
+//       } else {
+//         serverErr = true;
+//       }
+//     });
 
-  if (notFound) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (notFound) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  if (serverErr) {
-    return {
-      props: {
-        error: true,
-      },
-    };
-  }
+//   if (serverErr) {
+//     return {
+//       props: {
+//         error: true,
+//       },
+//     };
+//   }
 
-  return {
-    props: {
-      error: false,
-      token,
-    },
-  };
-};
+//   return {
+//     props: {
+//       error: false,
+//       token,
+//     },
+//   };
+// };
 
 export default VerificationLogin;
--- pages/partner/signin/index.tsx
+++ pages/partner/signin/index.tsx
@@ -13,6 +13,7 @@ import {
   Anchor,
   Stack,
   Loader,
+  Divider,
 } from "@mantine/core";
 import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
@@ -21,6 +22,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { signinWithGoogle } from "@/utils/auth";
 import { notifications } from "@mantine/notifications";
 import Link from "next/link";
+import Image from "next/image";
 
 const getCharacterValidationError = (str: string) => {
   return `Your password must have at least 1 ${str} character`;
@@ -40,27 +42,13 @@ function PartnerSignin(props: PaperProps) {
     password:
       type === "login"
         ? Yup.string().required("Password is required")
-        : Yup.string()
-            // check minimum characters
-            .min(8, "Password must have at least 8 characters")
-            // different error messages for different requirements
-            .matches(/[0-9]/, getCharacterValidationError("digit"))
-            .matches(/[a-z]/, getCharacterValidationError("lowercase"))
-            .matches(/[A-Z]/, getCharacterValidationError("uppercase"))
-            .required("Password is required"),
-
-    retypePassword:
-      type === "login"
-        ? Yup.string()
-        : Yup.string().oneOf([Yup.ref("password"), ""], "Passwords must match"),
+        : Yup.string(),
   });
 
   const form = useForm({
     initialValues: {
       email: "",
-      name: "",
       password: "",
-      retypePassword: "",
     },
 
     validate: yupResolver(schema),
@@ -101,10 +89,10 @@ function PartnerSignin(props: PaperProps) {
         const response = await axios.post(
           `${process.env.NEXT_PUBLIC_baseURL}/rest-auth/registration/`,
           {
-            first_name: form.values.name,
+            first_name: "",
             email: form.values.email,
-            password1: form.values.password,
-            password2: form.values.retypePassword,
+            password1: "",
+            password2: "",
             is_partner: true,
           }
         );
@@ -144,158 +132,160 @@ function PartnerSignin(props: PaperProps) {
   });
 
   return (
-    <Paper
-      w={"400px"}
-      mx={"auto"}
-      mt={12}
-      radius="md"
-      p="xl"
-      withBorder
-      {...props}
-    >
-      {loginError ? (
-        <div className="text-white sticky top-0 z-40 right-0 w-full mb-4 text-sm py-3 rounded-lg text-center px-4 bg-red-500 font-bold">
-          We couldn’t find an account matching the email or password you
-          entered. Please check your email or password and try again.
+    <>
+      <div className="border-b border-x-0 w-full h-[70px] border-t-0 flex items-center bg-gray-50 justify-between px-6 border-solid border-b-gray-200">
+        <div className="relative w-28 h-9 cursor-pointer">
+          <Image
+            alt="Winda logo"
+            src="/images/winda_logo/horizontal-blue-font.png"
+            className="w-full h-full"
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+            fill
+            priority
+          />
         </div>
-      ) : null}
 
-      {notAPartnerError ? (
-        <div className="text-white sticky top-0 z-40 right-0 w-full mb-4 text-sm py-3 rounded-lg text-center px-4 bg-red-500 font-bold">
-          User is not a lodge. Please register as a lodge. Go to the{" "}
-          <Link className="text-white" href={`/partner/signin/agent`}>
-            agent login page
-          </Link>{" "}
-          if you are trying to login as an agent.
-        </div>
-      ) : null}
+        <div></div>
+      </div>
 
-      <Text mb={12} size="lg" weight={700}>
-        Welcome to Winda, {type} with
-      </Text>
+      <div className="flex flex-col items-center overflow-y-scroll mt-[40px] justify-center">
+        <div className="w-[400px]">
+          {loginError ? (
+            <div className="text-white sticky top-0 z-40 right-0 w-full mb-4 text-sm py-3 rounded-lg text-center px-4 bg-red-500 font-bold">
+              We couldn’t find an account matching the email or password you
+              entered. Please check your email or password and try again.
+            </div>
+          ) : null}
 
-      <form onSubmit={form.onSubmit(() => {})}>
-        <Stack spacing={4}>
-          {type === "register" && (
-            <TextInput
-              label="Partner's name"
-              placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) =>
-                form.setFieldValue("name", event.currentTarget.value)
-              }
-              radius="md"
-            />
+          {notAPartnerError ? (
+            <div className="text-white sticky top-0 z-40 right-0 w-full mb-4 text-sm py-3 rounded-lg text-center px-4 bg-red-500 font-bold">
+              User is not a lodge. Please register as a lodge. Go to the{" "}
+              <Link className="text-white" href={`/partner/signin/agent`}>
+                agent login page
+              </Link>{" "}
+              if you are trying to login as an agent.
+            </div>
+          ) : null}
+
+          {type === "login" && (
+            <Text className="text-2xl mb-4" weight={700}>
+              Sign in to your account
+            </Text>
           )}
 
-          <TextInput
-            required
-            label="Email"
-            placeholder="hello@gmail.com"
-            value={form.values.email}
-            onChange={(event) =>
-              form.setFieldValue("email", event.currentTarget.value)
-            }
-            error={form.errors.email}
-            radius="md"
-          />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) =>
-              form.setFieldValue("password", event.currentTarget.value)
-            }
-            error={form.errors.password}
-            radius="md"
-          />
-
           {type === "register" && (
-            <PasswordInput
-              required
-              label="Re-type new password"
-              placeholder="Re-type new password"
-              value={form.values.retypePassword}
-              onChange={(event) =>
-                form.setFieldValue("retypePassword", event.currentTarget.value)
-              }
-              error={form.errors.retypePassword}
-              radius="md"
-            />
+            <Text className="text-2xl mb-4" weight={700}>
+              Create your free account
+            </Text>
           )}
-        </Stack>
 
-        <Group position="apart" mt="xl">
-          <Anchor
-            component="button"
-            type="button"
-            color="dimmed"
-            onClick={() => toggle()}
-            size="xs"
-          >
-            {type === "register"
-              ? "Already have an account? Login"
-              : "Don't have an account? Register"}
-          </Anchor>
+          <form onSubmit={form.onSubmit(() => {})}>
+            <Stack spacing={4}>
+              <TextInput
+                required
+                label="Email"
+                placeholder="hello@gmail.com"
+                size="lg"
+                value={form.values.email}
+                onChange={(event) =>
+                  form.setFieldValue("email", event.currentTarget.value)
+                }
+                error={form.errors.email}
+                radius="sm"
+              />
+
+              {type === "login" && (
+                <PasswordInput
+                  required={type === "login"}
+                  label="Password"
+                  placeholder="Your password"
+                  size="lg"
+                  mt={4}
+                  value={form.values.password}
+                  onChange={(event) =>
+                    form.setFieldValue("password", event.currentTarget.value)
+                  }
+                  error={form.errors.password}
+                  radius="sm"
+                />
+              )}
+            </Stack>
+
+            <div className="flex items-center gap-3 mt-4 flex-col">
+              <Button
+                onClick={() => submit()}
+                color="red"
+                type="submit"
+                size="lg"
+                radius="sm"
+                className="w-full"
+                disabled={isLoading}
+              >
+                {type === "login" ? "Login" : "Continue"}
+                {isLoading && <Loader size="xs" color="gray" ml={5}></Loader>}
+              </Button>
+              <Anchor
+                component="button"
+                type="button"
+                color="dimmed"
+                onClick={() => toggle()}
+                size="xs"
+              >
+                {type === "register"
+                  ? "Already have an account? Login"
+                  : "Don't have an account? Register"}
+              </Anchor>
+            </div>
+          </form>
+
+          <Divider labelPosition="center" my={8} label="or" />
+
           <Button
-            onClick={() => submit()}
-            color="red"
-            type="submit"
-            radius="xl"
-            className="flex items-center"
-            disabled={isLoading}
+            onClick={() => {
+              googleSocialLogin();
+            }}
+            loading={googleLoading}
+            size="lg"
+            mt={10}
+            leftIcon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                aria-hidden="true"
+                role="img"
+                width="24px"
+                height="24px"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#FFC107"
+                  d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
+                />
+                <path
+                  fill="#FF3D00"
+                  d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"
+                />
+                <path
+                  fill="#4CAF50"
+                  d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
+                />
+                <path
+                  fill="#1976D2"
+                  d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
+                />
+              </svg>
+            }
+            variant="default"
+            color="gray"
+            w={"100%"}
           >
-            {upperFirst(type)}
-            {isLoading && <Loader size="xs" color="gray" ml={5}></Loader>}
+            Continue with Google
           </Button>
-        </Group>
-      </form>
 
-      <Button
-        onClick={() => {
-          googleSocialLogin();
-        }}
-        loading={googleLoading}
-        mt={10}
-        leftIcon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="24px"
-            height="24px"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 48 48"
-          >
-            <path
-              fill="#FFC107"
-              d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
-            />
-            <path
-              fill="#FF3D00"
-              d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"
-            />
-            <path
-              fill="#4CAF50"
-              d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
-            />
-            <path
-              fill="#1976D2"
-              d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
-            />
-          </svg>
-        }
-        variant="default"
-        color="gray"
-        w={"100%"}
-      >
-        Continue with Google
-      </Button>
-
-      {/* <Anchor
+          {/* <Anchor
         component="button"
         type="button"
         color="dimmed"
@@ -308,7 +298,9 @@ function PartnerSignin(props: PaperProps) {
       >
         Forgot password?
       </Anchor> */}
-    </Paper>
+        </div>
+      </div>
+    </>
   );
 }
 
