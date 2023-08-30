9f14cabea949c95a23676846b5fe09606693d5d2
cliffordaustin <cliffordaustin670@gmail.com>
2023-08-30 18:10:48 +0000

    changes in code

--- pages/partner/signin/[email]/index.tsx
+++ pages/partner/signin/[email]/index.tsx
@@ -110,7 +110,7 @@ function PartnerSignin(props: PaperProps) {
       try {
         setLoading(true);
         const response = await axios.post(
-          `${process.env.NEXT_PUBLIC_baseURL}/rest-auth/registration/`,
+          `${process.env.NEXT_PUBLIC_baseURL}/user/registration/create/`,
           {
             first_name: form.values.name,
             email: form.values.email,
@@ -120,11 +120,10 @@ function PartnerSignin(props: PaperProps) {
           }
         );
 
-        const email = Buffer.from(form.values.email, "binary").toString(
-          "base64"
-        );
+        const token = response.data.key;
+        Cookies.set("token", token);
         router.push({
-          pathname: `/accounts/email-confirm/${email}`,
+          pathname: `/partner/lodge`,
         });
       } catch (error) {
         setLoading(false);
@@ -136,23 +135,6 @@ function PartnerSignin(props: PaperProps) {
       }
     }
   };
-
-  const [googleLoading, setGoogleLoading] = React.useState(false);
-
-  const googleSocialLogin = useGoogleLogin({
-    onSuccess: async (user) => {
-      setGoogleLoading(true);
-      await signinWithGoogle(user.access_token);
-      router.push((router.query.redirect as string) || "/partner/agent");
-    },
-    onError: (error) => {
-      setGoogleLoading(false);
-      notifications.show({
-        title: "Error",
-        message: "Something went wrong",
-      });
-    },
-  });
 
   return (
     <Paper
@@ -264,62 +246,6 @@ function PartnerSignin(props: PaperProps) {
           </Button>
         </Group>
       </form>
-
-      <Button
-        onClick={() => {
-          googleSocialLogin();
-        }}
-        loading={googleLoading}
-        mt={10}
-        leftIcon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="24px"
-            height="24px"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 48 48"
-          >
-            <path
-              fill="#FFC107"
-              d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
-            />
-            <path
-              fill="#FF3D00"
-              d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"
-            />
-            <path
-              fill="#4CAF50"
-              d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
-            />
-            <path
-              fill="#1976D2"
-              d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
-            />
-          </svg>
-        }
-        variant="default"
-        color="gray"
-        w={"100%"}
-      >
-        Continue with Google
-      </Button>
-
-      {/* <Anchor
-        component="button"
-        type="button"
-        color="dimmed"
-        onClick={() => {
-          router.push("/accounts/password-reset");
-        }}
-        size="xs"
-        mt={6}
-        className="text-center"
-      >
-        Forgot password?
-      </Anchor> */}
     </Paper>
   );
 }
